65e7ead5bf7982676167a13012f53dabe8cd7573
Meredyth Houpos <meredyth.houpos@gmail.com>
2024-05-17 14:24:50 -0400

    make nav button work

--- src/app/components/top-navigation.tsx
+++ src/app/components/top-navigation.tsx
@@ -1,22 +1,35 @@
+import { useState } from 'react';
 import Button from './buttons/button';
 import styles from './top-navigation.module.scss';
+import Modal from './modal';
 
 export default function TopNavigation() {
+	const [showModal, setShowModal] = useState(false);
+
+	const handleClick = () => {
+		setShowModal(true);
+	};
 	return (
-		<nav className={styles.navigationContainer}>
-			<div>
-				<img
-					src="#"
-					className={styles.logo}
-				/>
-			</div>
-			<div className={styles.profileContainer}>
-				<Button
-					handleClick={() => console.log('button clicked')}
-					title="Create a design"
-				/>
-				<img src="#" />
-			</div>
-		</nav>
+		<>
+			<Modal
+				isShowing={showModal}
+				willClose={() => setShowModal(false)}
+			/>
+			<nav className={styles.navigationContainer}>
+				<div>
+					<img
+						src="#"
+						className={styles.logo}
+					/>
+				</div>
+				<div className={styles.profileContainer}>
+					<Button
+						handleClick={handleClick}
+						title="Create a design"
+					/>
+					<img src="#" />
+				</div>
+			</nav>
+		</>
 	);
 }
