037c89b47b021b3df53a08a2fb81474c7b6818f7
Meredyth Houpos <meredyth.houpos@gmail.com>
2024-05-21 15:34:14 -0400

    begin moving UI and separating server side from client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-	return <div>log in coming soon</div>;
+import { getAllProjects } from '@/api/projects';
+import styles from './page.module.scss';
+import Cards from './components/cards';
+
+export default async function Home() {
+	const allProjects = await getAllProjects();
+	return (
+		<main className={styles.dashboardContainer}>
+			{!allProjects ? (
+				<div className={styles.noProjects}>
+					<img src="#" />
+					<span className={styles.title}>
+						You currently don't have any projects!
+					</span>
+					<span className={styles.subTitle}>
+						Click the button below and get started.
+					</span>
+					{/* <Button
+						title="Create a design"
+						handleClick={handleClick}
+					/> */}
+				</div>
+			) : (
+				<Cards projects={allProjects} />
+			)}
+		</main>
+	);
 }
