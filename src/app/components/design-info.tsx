d5338f9bcbac980b950cf5388c0b76fc3034bc23
Meredyth Houpos <meredyth.houpos@gmail.com>
2024-05-24 10:17:11 -0400

    move design info into its own client component

---
+++ src/app/components/design-info.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { useAppContext } from '../contexts/context';
+import styles from './design-info.module.scss';
+
+export default function DesignInformation() {
+	const { state } = useAppContext();
+	console.log('state', state);
+
+	if (!state || !state.currentProject) return null;
+	return (
+		<>
+			<h1>{state.currentProject.title}</h1>
+			<div>
+				{state.currentProject.height} x {state.currentProject.width}
+			</div>
+			<div
+				className={styles.currentColor}
+				style={{ background: state.selectedColor?.hex || 'transparent' }}></div>
+		</>
+	);
+}
